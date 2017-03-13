8e7f3b692e09c2e435dc5822860e79095aa25bb2
timo terpstra <timoski@outlook.com>
2017-03-13 19:53:15 +0100

    mid work update
    ben de css aan het bouwen en die voor de html aan het verwerken.

--- index.php
+++ index.php
@@ -5,16 +5,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/stylesheet.css">
     <title>kitten-race</title>
 </head>
 <body>
 <div class="wrapper">
-    <header></header>
-    <div class="content">
-        <div class="appdisplay"></div>
-        <div class="loginandregisterdisplay"></div>
+    <header class="bg-color-content row-spaced"></header>
+    <div class="content row-spaced">
+        <div class="appdisplay bg-color-content"></div>
+        <div class="loginandregisterdisplay bg-color-content">
+            <div class="loginsysteem">
+                <h3>Login</h3>
+            </div>
+            <div class="registreersysteem">
+                <h3>Resgistreer</h3>
+            </div>
+        </div>
     </div>
-    <footer></footer>
+    <footer class="bg-color-content row-spaced"></footer>
 </div>
 </body>
 </html>
