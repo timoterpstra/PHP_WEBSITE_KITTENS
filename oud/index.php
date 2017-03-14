b04e7ffa1123c2efee831959ce158b44461369ac
VerusLucian <minelukasz@gmail.com>
2017-03-14 16:06:00 +0100

    Web new version
    -php ready

---
+++ oud/index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<title>kitten-race</title>
+</head>
+<body>
+  <div class="wrapper">
+    <div class="hero">
+      <div class="appdisplay bg_color_white">
+        <img src="img/logo.png" alt="logo">
+        <h1>Kittens</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <!-- <a class="button" href="">Download</a> -->
+      </div>
+      <div class="login_reg_display bg_color_white">
+        <div class="login_reg_systeem">
+          <h3>Login</h3>
+          <form action="">
+            <div class="from_group">
+              <label class="label" for="usernameoremail">Username Or E-mail: </label>
+              <input class="input" name="usernameoremail" type="text" id="usernameoremail">
+            </div>
+            <div class="from_group">
+              <label class="label" for="password">Password: </label>
+              <input class="input" name="password" type="text" id="password">
+            </div>
+            <input class="submit" type="submit" value="Login">
+          </form>
+        </div>
+        <div class="login_reg_systeem">
+          <h3>Resgistreer</h3>
+          <form action="" method="">
+            <div class="from_group">
+              <label class="label" for="voornaam">Voornaam: </label>
+              <input class="input" name="voornaam" type="text" id="voornaam">
+            </div>
+            <div class="from_group">
+              <label class="label" for="achternaam">Achternaam: </label>
+              <input class="input" name="achternaam" type="text" id="achternaam">
+            </div>
+            <div class="from_group">
+              <label class="label" for="gebortedatum">Gebortedatum: </label>
+              <input class="input" name="gebortedatum" type="text" id="gebortedatum">
+            </div><div class="from_group">
+              <label class="label" for="land">Land: </label>
+              <input class="input" name="land" type="text" id="land">
+            </div>
+            <div class="from_group">
+              <label class="label" for="username">Username </label>
+              <input class="input" name="username" type="text" id="username">
+            </div><div class="from_group">
+              <label class="label" for="password">Password: </label>
+              <input class="input" name="password" type="text" id="password">
+            </div>
+            <input class="submit" type="submit" value="Register">
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
