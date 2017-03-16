6572c7b386ba250159574b252e9dc7cd8ab8db3a
timo terpstra <timoski@outlook.com>
2017-03-16 09:27:35 +0100

    update van 16-3-2017

--- index.php
+++ index.php
@@ -6,11 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/stylesheet.css">
+    <?php
+    $login = false;
+    ?>
     <title>kitten-race</title>
 </head>
 <body>
 <div class="wrapper">
-    <header class="bg-color-content row-spaced"></header>
+    <header class="bg-color-content row-spaced row-center column-center">
+        <img src="img/logo.png" alt="logo kittens">
+        <h1><span>kittens the </span>g<span>ame</span></h1>
+    </header>
     <div class="content">
         <div class="appdisplay bg-color-content">
             <h3>The Game</h3>
@@ -21,11 +27,21 @@
             <li>*3* -- text</li>
             <h3>How to download</h3>
             <p>the way to download is very easy. first login and if jou are logind dan you kan press te downlaud butten en alles zal van zelf gaan.</p>
+            <?php
+            if ($login)
+            {
+                echo "
+                    <div class=\"row-center\">
+                    <a href=\"php/download/test.zip\"><h2 class=\"download row-center\">Download</h2></a>
+                    </div>
+                ";
+            }
+            ?>
         </div>
         <div class="loginandregisterdisplay bg-color-content">
             <div class="loginsysteem">
                 <h3>Login</h3>
-                <form action="">
+                <form action="php/login/login.php" method="post">
                     <div class="row-spaced">
                         <label class="label" for="usernameoremail">Username Or E-mail: </label>
                         <input class="input" name="usernameoremail" type="text" id="usernameoremail">
@@ -39,7 +55,7 @@
             </div>
             <div class="registreersysteem">
                 <h3>Register</h3>
-                <form action="">
+                <form action="" method="post">
                     <div class="row-spaced">
                         <label class="label" for="name">Name: </label>
                         <input class="input" name="name" type="text" id="name">
---
+++ php/login/login.php
@@ -0,0 +1,20 @@
+<?php
+include "../db_connect/connect.php";
+$dbc = _Connect_db();
+
+$usernameOrEmail = $_POST['usernameoremail'];
+$password = $_POST['password'];
+
+if (($usernameOrEmail != null || $usernameOrEmail != "") || ($password != null || $password != ""))
+{
+    $resuld = mysqli_query($dbc, "SELECT * FROM person WHERE gebruikersnaam = $usernameOrEmail AND wachtwoord = $password OR email = $usernameOrEmail AND wachtwoord = $password");
+    if ($resuld)
+    {
+        echo "ja lukt";
+    }
+    else
+    {
+        echo "nee lukt niet";
+        var_dump($resuld);
+    }
+}
